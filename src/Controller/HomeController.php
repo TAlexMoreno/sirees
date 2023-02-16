bf42518dbb590be0ca93e8cbd6d4ba1c912c62e1
Alejandro Moreno Tapia <alex.mt.10@gmail.com>
2023-02-16 15:09:42 -0600

    basicamente nada

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,13 +1,16 @@
 <?php
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController {
-
+class HomeController extends AbstractController
+{
     #[Route("/", "app_home")]
-    public function appHome(){
+    public function appHome()
+    {
+        $this->denyAccessUnlessGranted("ROLE_USER");
         return $this->render("home/index.html.twig");
     }
 }
